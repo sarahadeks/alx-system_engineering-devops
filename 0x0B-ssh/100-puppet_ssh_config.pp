28e542b382dc34e569a5b116167c2b1278f048d5
sarahadeks <sarahopeyemi@yahoo.com>
2023-09-10 22:11:21 -0700

    using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# using puppet to mae changes to the configuration file
+
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
